91a1b8b0339a2bb47cf9615c97e5a4d1d2956896
yhj <yuhejian000@163.com>
2020-09-08 17:14:54 +0800

    项目初步搭建

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,34 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Router, { RouteConfig } from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router);
 
-const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+/**
+ * hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
+ * alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
+ *                                if not set alwaysShow, only more than one route under the children
+ *                                it will becomes nested mode, otherwise not show the root menu
+ * redirect: noredirect           if `redirect:noredirect` will no redirct in the breadcrumb
+ * name:'router-name'             the name is used by <keep-alive> (must set!!!)
+ * meta : {
+    title: 'title'               the name show in submenu and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar,
   }
-]
+ **/
 
-const router = new VueRouter({
-  routes
-})
+/** 公共路由 首页和登录页和一些不用权限的公用页面 */
+const constantRouterMap: RouteConfig[] = [
+  {
+    path: '/404',
+    component: () => import('@/views/404.vue'),
+    meta: {
+      hidden: true
+    }
+  }
+];
+
+const router = new Router({
+  routes: constantRouterMap
+});
 
 export default router
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,9 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
+
+/** 加入ts检查 cookie 管理工具 */
+declare module 'js-cookie'
+
+/** 声明图片 */
+declare module '*.gif'
